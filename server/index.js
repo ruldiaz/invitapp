4ed7cd3ae6e8c83637cf3a819fcd0255e5b9657e
ruldiaz <rulhdiaz@gmail.com>
2024-09-01 22:19:32 -0600

    commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
