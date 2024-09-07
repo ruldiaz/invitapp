bb826100545be1bde899b4250fc89ff8647a4dbb
ruldiaz <rulhdiaz@gmail.com>
2024-09-06 21:59:04 -0600

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 import { Login } from './components/Login'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/Navbar/Navbar';
 import Profile from './components/Profile';
 import Register from './components/Register';
+import { Landing } from './components/Landing';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route element={<Landing />} path='/' />
         <Route element={<Login />} path='/login' />
         <Route element={<Profile />} path='/profile' />
         <Route element={<Register />} path='/register' />
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/register'>Register</Link></li>
+      </ul>
+    </nav>
+  );
+};
