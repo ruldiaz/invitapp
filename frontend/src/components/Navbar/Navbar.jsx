8c6291122de595efabd2e664db8143556651cd86
ruldiaz <rulhdiaz@gmail.com>
2024-09-06 22:20:50 -0600

    commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ export const Navbar = () => {
   return (
     <nav className="navbar">
       <ul>
+        <li><Link to='/'>Home</Link></li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/register'>Register</Link></li>
       </ul>
