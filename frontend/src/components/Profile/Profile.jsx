6bc3d647e8b73a0bf5bb264c7a68099db61d27fd
ruldiaz <rulhdiaz@gmail.com>
2024-09-08 18:05:03 -0600

    profile update function

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -3,7 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import './Profile.css';
 
 const Profile = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+  const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
 
@@ -41,6 +46,52 @@ const Profile = () => {
     fetchUserData();
   }, [navigate]);
 
+  const handleProfileUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/api/user/update',{
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify(user),
+      });
+      if(response.ok){
+        alert('Profile updated succesfully!');
+      }else{
+        const {msg} = await response.json();
+        throw new Error(msg);
+      }
+
+    } catch (error) {
+      alert(`Profile update failed: ${error?.message} || 'Unknown error'`);
+    }
+  };
+
+  const handlePasswordUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/api/user/update-password', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({password})
+      });
+      if(response.ok){
+        alert('Password updated succesfully');
+        setPassword('');
+      }else{
+        const {msg} = response.json();
+        throw new Error(msg);
+      }
+    } catch (error) {
+      alert(`Password update failed: ${error?.message || 'Unknown error'}`);
+    }
+  }
+
   if (errorMessage) {
     return <div>{errorMessage}</div>;
   }
@@ -53,9 +104,28 @@ console.log(user);
   return (
 
     <div className='profile-component' id="user-info">
-      <p>First name: {user.firstName}</p>
-      <p>Last name: {user.lastName}</p>
-      <p>Email: {user.email}</p>
+      <form onSubmit={handleProfileUpdate}>
+        <p>
+          First name:
+          <input type="text" value={user.firstName} onChange={(e)=>setUser({...user, firstName: e.target.value})} />
+        </p>
+        <p>
+          Last name:
+          <input type="text" value={user.lastName} onChange={(e)=>setUser({...user, lastName: e.target.value})} />
+        </p>
+        <p>
+          Email:
+          {user.email}
+        </p>
+        <button type='submit'>Update Profile</button>
+      </form>
+      <form onSubmit={handlePasswordUpdate}>
+        <p>
+          New password: 
+          <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+        </p>
+        <button type='submit'>Update Password</button>
+      </form>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
    origin: ['http://localhost:9000','http://localhost:5173'],
    credentials: true,
-   methods: ['GET','POST'],
+   methods: ['GET','POST', 'PUT'],
    allowHeaders: ['Content-Type','Authorization']
 }
 
