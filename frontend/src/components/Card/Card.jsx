30e97abea484adb5f04cee9b244f71b3ac74dc34
ruldiaz <rulhdiaz@gmail.com>
2024-09-18 19:56:41 -0600

    cards

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './dummyProducts';
 import { products } from './dummyProducts';
 import './Card.css';
 
 export const Card = () => {
-   console.log(JSON.stringify(products, null, 2));
-   
+  console.log(JSON.stringify(products, null, 2));
+  
   return (
     <div className='card-component'>
-    
-      {products.map((e)=>(
-         <li key={e.id}>
-            <p>{e.designName}</p>
-            <p>{e.price}</p>
-            <img src={e.image} width={150} height={150} alt={e.designName} />
-         </li>
+      {products.map((e) => (
+        <div key={e.id} className='card-item'>
+          <h3>{e.designName}</h3>
+          <h4>$ {e.price}</h4>
+          <img src={e.image} width={150} height={150} alt={e.designName} />
+        </div>
       ))}
-    
     </div>
-  )
-}
+  );
+};
