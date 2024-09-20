a700167376304b503e6f5cf6ad2b7bf788019745
ruldiaz <rulhdiaz@gmail.com>
2024-09-19 21:15:57 -0600

    commit

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,7 +4,8 @@ const productSchema = new mongoose.Schema({
    designName: {type: String, required: true},
    price: {type: Number, required: true},
    image: {type: String, required: true},
-   groomName: {type: String, required: true}
+   groomName: {type: String, required: true},
+   brideName: {type: String, required: true}
 });
 
 const Product = mongoose.model("Product", productSchema);
